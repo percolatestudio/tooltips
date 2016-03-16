c2458288271810faa6bbdb63bc1018da77056ea8
Tom Coleman <tom@thesnail.org>
2016-03-16 14:26:00 +1100

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'percolate:tooltips',
   summary: 'Basic qtip tooltips for Meteor',
-  version: '1.0.0',
+  version: '1.1.0',
   git: 'https://github.com/percolatestudio/tooltips.git'
 });
 
